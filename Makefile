13a71eda3d3b7be48699859dbf3e379d336f3d27
qingpengchen <qingpengchen2011@gmail.com>
2014-01-22 18:34:42 -0800

    a udp client(also can be used as a server, indicated in the last param,param 5,as 1)

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 #f your application is in a file named myapp.cpp or myapp.c
 # this is the line you will need to build the binary.
-all: sendmsg sendmsgTCP
+all: sendmsg_to_server sendmsg_to_peer
 
-sendmsg: sendmsg.c
+sendmsg_to_server: sendmsg_to_server.c
 	$(CC) -o $@ $< -g -DDEBUG -lpthread
-sendmsgTCP: sendmsgTCP.c
-	$(CC) -o $@ $< -g -DDEBUG
+sendmsg_to_peer: sendmsg_to_peer.c
+	$(CC) -o $@ $< -g -DDEBUG -lpthread
 clean: 
-	rm -f sendmsg sendmsg.o sendmsgTCP sendmsgTCP.o
+	rm -f sendmsg_to_peer sendmsg_to_peer.o sendmsg_to_server sendmsg_to_server.o
---
+++ sendmsg_to_server.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <sys/types.h>          
+#include <sys/socket.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+#define PJ_INET6_ADDRSTRLEN	46
+static int  UDPCreate();
+static void UDPCreateThread();
+static void* UDPRecvThread(void* arg);
+static void* UDPSendThread(void* arg);
+
+
+static struct app_t
+{
+    int rport;
+    char ripaddr[PJ_INET6_ADDRSTRLEN];
+    int lport;
+    char lipaddr[PJ_INET6_ADDRSTRLEN]; 
+    int localfd;
+
+    int server;
+
+   struct sockaddr_in addr_server,addr_local;
+   int len;
+} icedemo;
+
+
+static int  UDPCreate(){
+   int sfd;
+   int sent,on=1;
+  
+   icedemo.len=sizeof(struct sockaddr);
+   //icedemo_stop_session(); 
+   sfd=socket(AF_INET,SOCK_DGRAM,0);
+   if(sfd==-1){
+       perror("SOCKET");
+       return ;
+   }
+   setsockopt(sfd,SOL_SOCKET,SO_REUSEADDR | SO_BROADCAST,&on,sizeof(on)); 
+   memset(&icedemo.addr_server,0,icedemo.len);
+   icedemo.addr_server.sin_family=AF_INET;
+   icedemo.addr_server.sin_addr.s_addr=inet_addr(icedemo.ripaddr); 
+   icedemo.addr_server.sin_port=htons(icedemo.rport);
+
+   memset(&icedemo.addr_local,0,icedemo.len);
+   icedemo.addr_local.sin_family=AF_INET;
+   icedemo.addr_local.sin_addr.s_addr=inet_addr(icedemo.lipaddr); 
+   icedemo.addr_local.sin_port=htons(icedemo.lport);
+  
+   if(bind(sfd,(struct sockaddr*)&icedemo.addr_local,icedemo.len)==-1){
+    perror("BIND");
+    return ;
+    } 
+   /* 
+    if(connect(sfd,(struct sockaddr*)&addr_server,len)==-1){
+     perror("CONNECT");
+     return ;
+   }
+   */
+   printf("OUR OWN UDP SOCKET CREATED:%d\n",sfd);
+    return sfd;
+}
+static void UDPCreateThread(){
+  int rt;
+  pthread_t idsend,idrecv;
+ if(!icedemo.server){
+      rt=pthread_create(&idsend,NULL,UDPSendThread,NULL);
+      if(rt!=0){
+         perror("PTHREAD_CREATE UDPSendThread");
+      }
+  } 
+  else{
+  rt=pthread_create(&idrecv,NULL,UDPRecvThread,NULL);
+  if(rt!=0){
+    perror("PTHREAD_CREATE UDPRecvThread");
+ } 
+  }
+  if(!icedemo.server){
+  pthread_join(idsend,NULL);
+  }
+  else{
+  pthread_join(idrecv,NULL);
+  }
+}
+
+static void* UDPRecvThread(void* arg){
+  printf("UDPRecvThread CREATED\n");
+  char buffer[1024];
+  int n;
+  struct sockaddr_in addr_server;
+  int addr_len=sizeof(struct sockaddr_in);
+  for(;;){
+    //n=read(icedemo.localfd,buffer,sizeof(buffer));
+    n=recvfrom(icedemo.localfd,buffer,sizeof(buffer), 0 , (struct sockaddr *)&addr_server ,&addr_len);   
+    printf("receive from %s:%d\n" , inet_ntoa(addr_server.sin_addr),ntohs(addr_server.sin_port));
+    buffer[n]='\0';
+    printf("read %d bytes,data:%s\n",n,buffer); 
+
+    sleep(4);
+    
+   
+  
+  }
+}
+
+static void* UDPSendThread(void* arg){
+   int n;
+   char buffer[1024]="hello";
+   printf("UDPSendThread CREATED\n");
+   for(;;){
+   sleep(3);
+   //gets(&buffer); 
+    n=strlen(buffer);
+   //n=write(icedemo.localfd,buffer,n);
+   n=sendto(icedemo.localfd,buffer,n,0,(struct sockaddr*)&icedemo.addr_server,icedemo.len);
+   printf("%d byte to send,%d bytes sent\n",strlen(buffer),n);
+  }
+  
+
+}
+
+
+int main(int argc,char* argv[]){
+    sprintf(icedemo.lipaddr,"%s",argv[1]);
+    icedemo.lport=atoi(argv[2]);
+    sprintf(icedemo.ripaddr,"%s",argv[3]);
+    icedemo.rport=atoi(argv[4]);
+ 
+    icedemo.server=atoi(argv[5]);
+ 
+    icedemo.localfd=UDPCreate();
+    /* a thread to read and a thread to write*/
+     UDPCreateThread();
+   return 0;
+}
